246e275b21bb480fbdc76d90bc7d316261d279a9
Gabor Wnuk <gabor.wnuk@grupawp.pl>
2016-05-10 11:01:50 +0200

    Initial commit.

---
+++ Weather/Weather.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Weather
+{
+	public class App : Application
+	{
+		WeatherService service;
+
+		Label cityName;
+		Label dayName;
+
+		Label temperature;
+		Label pressure;
+		Label precipitation;
+
+		RelativeLayout footerLayout;
+
+		public App ()
+		{
+			this.service = new WeatherService();
+
+			// The root page of your application
+			MainPage = this.GetMainPage ();
+		}
+
+		public Page GetMainPage ()
+		{
+			RelativeLayout mainLayout = new RelativeLayout ();
+
+			mainLayout.Children.Add (new Image() {
+					Source = ImageSource.FromResource("Weather.Images.bg.png"),
+					Aspect = Aspect.AspectFill
+				}, 
+				Constraint.Constant (0),
+				Constraint.Constant (0),
+				Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Height; })
+			);
+
+			// Header layout
+			RelativeLayout headerLayout = new RelativeLayout ();
+			mainLayout.Children.Add (headerLayout,
+				Constraint.Constant (0),
+				Constraint.Constant (20),
+				Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+				Constraint.Constant (100));
+
+			headerLayout.Children.Add (new Image() {
+					Source = ImageSource.FromResource("Weather.Images.cloud.png"),
+				}, 
+				Constraint.RelativeToParent ((parent) => { return parent.Width / 2 - 80; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Height / 2 - 22; }),
+				Constraint.Constant (57),
+				Constraint.Constant (44));
+
+			this.cityName = new Label () {
+				Text = "WARSZAWA",
+				FontSize = 16,
+				TextColor = Color.White
+			};
+
+			headerLayout.Children.Add (this.cityName, 
+				Constraint.RelativeToParent ((parent) => { return parent.Width / 2 - 10; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Height / 2 - 11; }),
+				Constraint.Constant (100),
+				Constraint.Constant (18));
+
+			this.dayName = new Label () {
+				Text = "ŚRODA, 4 MAJ",
+				FontSize = 8,
+				TextColor = Color.White
+			};
+
+			headerLayout.Children.Add (this.dayName, 
+				Constraint.RelativeToParent ((parent) => { return parent.Width / 2 - 10; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Height / 2 + 7; }),
+				Constraint.Constant (100),
+				Constraint.Constant (10));
+			// >
+
+			// Footer layout
+			this.footerLayout = new RelativeLayout ();
+
+			mainLayout.Children.Add (this.footerLayout,
+				Constraint.Constant (0),
+				Constraint.RelativeToParent ((parent) => { return parent.Height - 120; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+				Constraint.Constant (120));
+			// >
+
+			// Degrees and central area layout
+			this.temperature = new Label () {
+				Text = "--°",
+				FontSize = 200,
+				TextColor = Color.White,
+				HorizontalTextAlignment = TextAlignment.Center,
+				VerticalTextAlignment = TextAlignment.Center
+			};
+
+			RelativeLayout degreesLayout = new RelativeLayout ();
+			mainLayout.Children.Add (degreesLayout,
+				Constraint.Constant (0),
+				Constraint.RelativeToView(headerLayout, (parent, sibling) => {
+					return sibling.Y + sibling.Height;
+				}), 
+				Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+				Constraint.RelativeToView(footerLayout, (parent, sibling) => {
+					return sibling.Y - sibling.Height;
+				}));
+
+			degreesLayout.Children.Add (this.temperature,
+				Constraint.Constant (0),
+				Constraint.Constant (0),
+				Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+				Constraint.RelativeToParent ((parent) => { return parent.Height / 1.5; }));
+			// >
+
+			// Platform specific actions
+			Device.OnPlatform (
+				iOS: () => {
+					this.temperature.FontFamily = "HelveticaNeue-UltraLight";
+					this.cityName.FontFamily = "HelveticaNeue-Light";
+				},
+				Android: () => {
+					this.temperature.FontFamily = "Sans-Serif-Thin";
+					this.cityName.FontFamily = "Sans-Serif-Light";
+				}
+			);
+			// >
+
+			return new ContentPage {
+				Content = mainLayout,
+			};
+		}
+
+		private async void refreshForecast() {
+			var today = DateTime.Now;
+			var dayOfWeekName = today.DayOfWeek.ToString ().ToUpper ();
+			var monthName = today.ToString("MMMM").ToUpper ();
+			this.dayName.Text = $"{dayOfWeekName}, {today.Day} {monthName}";
+
+			// Currently, as an example, we're using hardcoded Warsaw location
+			var forecast = await this.service.Fetch ("https://api.o2.pl/weather/api/o2/weather?cid=1201290&days=4");
+			var currentForecast = forecast.Details [0].Current;
+			this.temperature.Text = $"{currentForecast.Temperature}°";
+			this.cityName.Text = forecast.Name.ToUpper ();
+
+			// Forecast for coming days
+			var offset = 0;
+			List<string> cellColors = new List<string>(new string[] {
+				"#d9a9ce", "#d29cc7", "#c586ba", "ba75b1"
+			});
+			var iterationOffset = (int)this.footerLayout.Width / cellColors.Count;
+			for (int i = 0; i < cellColors.Count; i++) {
+				var dayForecast = forecast.Details [i];
+				var cellLayout = new RelativeLayout ();
+				cellLayout.BackgroundColor = Color.FromHex (cellColors [i]);
+
+				this.footerLayout.Children.Add (cellLayout,
+					Constraint.Constant (offset),
+					Constraint.Constant (0),
+					Constraint.Constant (iterationOffset),
+					Constraint.RelativeToParent ((parent) => {
+						return parent.Height;
+					}));
+
+				var dayName = new Label () {
+					Text = dayForecast.Date.DayOfWeek.ToString ().ToUpper (),
+					FontSize = 8,
+					TextColor = Color.White,
+					HorizontalTextAlignment = TextAlignment.Center,
+					VerticalTextAlignment = TextAlignment.Center
+				};
+
+				cellLayout.Children.Add (dayName, 
+					Constraint.Constant (0),
+					Constraint.Constant (18),
+					Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+					Constraint.Constant (10));
+
+				var dayTemperature = new Label () {
+					Text = $"{dayForecast.Forecast.Temperature}°",
+					FontSize = 22,
+					TextColor = Color.White,
+					HorizontalTextAlignment = TextAlignment.Center,
+					VerticalTextAlignment = TextAlignment.Center
+				};
+
+				cellLayout.Children.Add (dayTemperature, 
+					Constraint.Constant (3),
+					Constraint.RelativeToParent ((parent) => { return parent.Height - 37; }),
+					Constraint.RelativeToParent ((parent) => { return parent.Width; }),
+					Constraint.Constant (18));
+
+				cellLayout.Children.Add (new Image() {
+						Source = ImageSource.FromResource("Weather.Images.small_cloud.png"),
+					}, 
+					Constraint.RelativeToParent ((parent) => { return parent.Width / 2 - 12; }),
+					Constraint.RelativeToParent ((parent) => { return parent.Height / 2 - 15; }),
+					Constraint.Constant (24),
+					Constraint.Constant (22));
+
+				// Platform specific actions
+				Device.OnPlatform (
+					iOS: () => {
+						dayTemperature.FontFamily = "HelveticaNeue-UltraLight";
+						dayName.FontFamily = "HelveticaNeue-Light";
+					},
+					Android: () => {
+						dayTemperature.FontFamily = "Sans-Serif-Thin";
+						dayName.FontFamily = "Sans-Serif-Light";
+					}
+				);
+				// >
+
+				offset += iterationOffset;
+			}
+			// >
+		}
+
+		protected override void OnStart ()
+		{
+			this.refreshForecast ();
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+			this.refreshForecast ();
+		}
+	}
+}
+
---
+++ Weather/WeatherService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Http;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+using System.Threading.Tasks;
+
+using ModernHttpClient;
+
+namespace Weather
+{
+	// Simple JSON deserializer. We've implemented serialization for fields
+	// we actually require for our purposes (that's why some of them are missing
+	// comparing to API response).
+	[DataContract]
+	public class WeatherForecast {
+		[DataMember(Name="cityName")] public string Name { get; set; }
+		[DataMember(Name="days")] public IList<WeatherForecastDay> Details { get; set; }
+	}
+
+	[DataContract]
+	public class WeatherForecastDay {
+		[DataMember(Name="sunrise")] public string Sunrise { get; set; }
+		[DataMember(Name="sunset")] public string Sunset { get; set; }
+		[DataMember(Name="date")] public int Timestamp { get; set; }
+		public DateTime Date {
+			get {
+				return new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(this.Timestamp).ToLocalTime();
+		}}
+		[DataMember(Name="timeOfDay")] public IList<WeatherForecastHour> Hours { get; set; }
+		public WeatherForecastHour Current { 
+			get {
+				Int32 currentTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+				WeatherForecastHour currentForecast = this.Hours [0];
+				var currentDifference = Math.Abs (currentTimestamp - currentForecast.Timestamp);
+
+				foreach (WeatherForecastHour hourForecast in this.Hours) {
+					var difference = Math.Abs (currentTimestamp - hourForecast.Timestamp);
+					if (Math.Abs (currentTimestamp - hourForecast.Timestamp) < currentDifference) {
+						currentDifference = difference;
+						currentForecast = hourForecast;
+					}
+				}
+
+				return currentForecast;
+			}
+		}
+		public WeatherForecastHour Forecast { 
+			get {
+				WeatherForecastHour highestTemperatureForecast = this.Hours [0];
+				int highestTemperature = -255;
+				foreach (WeatherForecastHour hourForecast in this.Hours) {
+					if (hourForecast.Temperature > highestTemperature) {
+						highestTemperature = hourForecast.Temperature;
+						highestTemperatureForecast = hourForecast;
+					}
+				}
+
+				return highestTemperatureForecast;
+			}
+		}
+	}
+
+	[DataContract]
+	public class WeatherForecastHour {
+		[DataMember(Name="temperature")] public int Temperature { get; set; }
+		[DataMember(Name="icon")] public string Icon { get; set; }
+		[DataMember(Name="precipitation")] public string Precipitation { get; set; }
+		[DataMember(Name="pressure")] public int Pressure { get; set; }
+		[DataMember(Name="hour")] public int Timestamp { get; set; }
+	}
+
+	public class WeatherService {
+		public WeatherService() {
+		}
+
+		public async Task<WeatherForecast> Fetch(string url) {
+			var serializer = new DataContractJsonSerializer(typeof(WeatherForecast));
+
+			// We're using NativeMessageHandler from ModernHttpClient to utilize
+			// platform specific connection managers (NSURLSession for iOS and OKHttp for Android).
+			HttpClient client = new HttpClient (new NativeMessageHandler());
+
+			Stream stream = await client.GetStreamAsync (url);
+			WeatherForecast forecast = (WeatherForecast)serializer.ReadObject (stream);
+			return forecast;
+		}
+	}
+}
+
